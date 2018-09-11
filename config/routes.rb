a6560161a418157d82d724423f5a8f39fdaace5f
Adam Long <adamlong1066@gmail.com>
2018-09-11 17:37:57 -0400

    Push Functioning app to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:create, :show]
     resource :session, only: [:create, :destroy]
   end
+
+    root "static_pages#root"
 end
