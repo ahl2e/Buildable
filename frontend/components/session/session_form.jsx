490195d486a90dde525befe6587488381a7d275b
Adam Long <adamlong1066@gmail.com>
2018-09-13 11:45:38 -0400

    Add email field to signup form

--- frontend/components/session/session_form.jsx
+++ frontend/components/session/session_form.jsx
@@ -21,9 +21,29 @@ handleSubmit(e) {
   e.preventDefault();
   const user = Object.assign({}, this.state);
   this.props.dispatchForm(user);
+  this.setState({
+    username: "",
+    password: "",
+    email:""
+  });
 }
 
   render(){
+    const isSignUp = this.props.formType;
+ let emailField;
+
+ if ( isSignUp ==="SIGN UP") {
+   emailField =
+   <label>Email
+   <input
+     type="text"
+     value={this.state.email}
+     onChange={this.update('email')}
+     />
+ </label>;
+ } else {
+   emailField = <div className="no-show"></div>
+ }
     return(
       <div>
         <div>{this.props.formType} or {this.props.navLink}</div>
@@ -32,9 +52,11 @@ handleSubmit(e) {
           <input type="text" value={this.state.username} onChange={this.update('username').bind(this)} />
         </label>
       <br/>
-          <label>Password
+        <label>Password
           <input type="password" value={this.state.password} onChange={this.update('password')} />
         </label>
+        <br/>
+        {emailField}
         <input type="submit" value ={this.props.formType}></input>
         </form>
       </div>
