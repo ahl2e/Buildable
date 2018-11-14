863fcf3a805e60ee848b6828ed19f9d0e7ee0e13
Adam Long <adamlong1066@gmail.com>
2018-11-14 14:22:48 -0500

    Remove debuggers

--- frontend/components/comments/comments_index.jsx
+++ frontend/components/comments/comments_index.jsx
@@ -31,7 +31,6 @@ class CommentsIndex extends React.Component{
 
   render() {
     if (this.state.comments.length > 0){
-      debugger
       return this.state.comments.map((comment,idx) => {
         return (
           <div>
