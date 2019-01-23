f74d51b76875a9a84f5678f82cde37203f0a3cd9
Adam Long <adamlong1066@gmail.com>
2019-01-23 16:43:30 -0500

    Restyle ctegories index and add label

--- frontend/components/projects/category_index.jsx
+++ frontend/components/projects/category_index.jsx
@@ -3,16 +3,22 @@ import CategoryIndexItem from './category_index_item';
 import {Link} from 'react-router-dom';
 
 const CategoryIndex = (props) =>{
+  var projects = props.projects.map(project => {
+    return (<CategoryIndexItem key={project.id} project={project}/>);
+  });
+
   if(props.projects.length > 0){
-    return props.projects.map((project,idx) => {
+    var label = parseHistory(props.history.location.pathname);
       return (
         <div>
-          <ul>
-            <CategoryIndexItem key={idx} project={project}/>
+          <div id='category-label-container'>
+            <h1 id='category-label'>{label}</h1>
+          </div>
+          <ul className='projects-index-list'>
+            {projects}
           </ul>
         </div>
       )
-    })
   }else{
     return(
       <div>
@@ -22,5 +28,15 @@ const CategoryIndex = (props) =>{
     )
   }
 };
+
+
+const parseHistory = (str) => {
+  var arr = str.split("/");
+  if (arr[arr.length -1].split('_').length > 1){
+    return arr[arr.length -1].split('_').join(" ");
+  }else{
+    return arr[arr.length -1];
+  }
+}
 
 export default CategoryIndex;
